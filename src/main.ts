d5e15ae833de5c72dcc8deea6b247cd2f5733fbc
Miad Abdi <50056491+miadabdi@users.noreply.github.com>
2024-04-09 00:22:05 +0330

    chore(setup): add cookie-parser, hpp, helmet

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import * as hpp from 'hpp';
 import { AppModule } from './app.module';
 import { logger } from './winston';
 
@@ -6,6 +9,13 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
 		logger,
 	});
+
+	app.use(helmet());
+	app.use(cookieParser());
+	app.use(hpp());
+
+	app.enableCors();
+
 	await app.listen(3000);
 }
 bootstrap();
