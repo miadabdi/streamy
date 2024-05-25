077b3842361fba15689b9eb073ff5bf21a03e917
Miad Abdi <50056491+miadabdi@users.noreply.github.com>
2024-05-25 22:03:24 +0330

    feat(auth): add public decorator to exclude some api from auth

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -1,6 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { JWT_STRATEGY_NAME } from '../../common/constants';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard(JWT_STRATEGY_NAME) {}
+export class JwtAuthGuard extends AuthGuard(JWT_STRATEGY_NAME) {
+	constructor(private readonly reflector: Reflector) {
+		super();
+	}
+
+	canActivate(context: ExecutionContext) {
+		const isPublic = this.reflector.get<boolean>('isPublic', context.getHandler());
+
+		if (isPublic) {
+			return true;
+		}
+
+		return super.canActivate(context);
+	}
+}
