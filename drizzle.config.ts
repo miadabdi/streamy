453b042460fb99b42a6875b015ed59202473a630
Miad Abdi <50056491+miadabdi@users.noreply.github.com>
2024-09-13 14:34:07 +0330

    chore(package.json): updated packages

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,21 @@
 import dotenv from 'dotenv';
-import 'dotenv/config';
+import dotenvExpand from 'dotenv-expand';
 import type { Config } from 'drizzle-kit';
 import { join } from 'path';
 
-dotenv.config({
-	path: join(__dirname, '../../app.env'),
-});
+const envFilePath = join(__dirname, '.env');
+
+dotenvExpand.expand(
+	dotenv.config({
+		path: envFilePath,
+	}),
+);
 
 export default {
 	schema: './src/drizzle/schema.ts',
 	out: './drizzle',
-	driver: 'pg', // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
+	dialect: 'postgresql',
 	dbCredentials: {
-		connectionString: process.env.DATABASE_URL,
+		url: process.env.DATABASE_URL,
 	},
 } satisfies Config;
--- src/drizzle/migrate.ts
+++ src/drizzle/migrate.ts
@@ -1,6 +1,16 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import { join } from 'path';
 import { createDbConnection } from './drizzle.provider';
+
+const envFilePath = join(__dirname, '../../.env');
+
+dotenvExpand.expand(
+	dotenv.config({
+		path: envFilePath,
+	}),
+);
 
 const { pool, db } = createDbConnection({ max: 1 });
 
