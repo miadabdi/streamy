6689fe55c1457e02e1fd1e43e9438485c4257497
Miad Abdi <miadabdi80@gmail.com>
2026-09-07 00:54:12 +0330

    chore(eslint): fix flat config to use flat-config keys
    the config mixed legacy parser/extends keys into flat config, which
    eslint 9 rejects at load time; npm run lint has been broken since it
    landed. rewrite using the same working shape as the process node repo

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,39 @@
-module.exports = {
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: 'tsconfig.json',
-		tsconfigRootDir: __dirname,
-		sourceType: 'module',
+import parser from '@typescript-eslint/parser';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierRecommended from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{
+		ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
 	},
-	plugins: ['@typescript-eslint/eslint-plugin'],
-	extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-	root: true,
-	env: {
-		node: true,
-		jest: true,
+	{
+		files: ['**/*.ts'],
+		languageOptions: {
+			parser,
+			parserOptions: {
+				project: 'tsconfig.json',
+				tsconfigRootDir: import.meta.dirname,
+				sourceType: 'module',
+			},
+		},
+		plugins: {
+			'@typescript-eslint': tseslint,
+			prettier: prettierPlugin,
+		},
+		rules: {
+			...tseslint.configs.recommended.rules,
+			...prettierRecommended.rules,
+			'prettier/prettier': 'error',
+			'@typescript-eslint/interface-name-prefix': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-require-imports': 'off',
+			'@typescript-eslint/no-unused-expressions': 'off',
+			'@typescript-eslint/no-empty-object-type': 'off',
+		},
 	},
-	ignorePatterns: ['.eslintrc.js'],
-	rules: {
-		'@typescript-eslint/interface-name-prefix': 'off',
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-unused-vars': 'off',
-	},
-};
+];
